19938a0677aab46264a60fb3236710aa0cf5635b
Matthew Pidden <bb22475@bristol.ac.uk>
2023-05-24 17:55:18 +0100

    Functional game

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Board.css'
+import ReviewCircles from './ReviewCircles';
 import Keyboard from './keyboard.js';
 
 
-function Board() {
+function Board({ currentBox }) {
 
-  var currentBox = 'box-1-1';
 
   return (
     <div className="board-container">
@@ -15,36 +15,42 @@ function Board() {
           <div className="box" id="box-1-2"></div>
           <div className="box" id="box-1-3"></div>
           <div className="box" id="box-1-4"></div>
+          <div className="reviewCircles1"><ReviewCircles /></div>
         </div>
         <div className="row" id="row-2">
           <div className="box" id="box-2-1"></div>
           <div className="box" id="box-2-2"></div>
           <div className="box" id="box-2-3"></div>
           <div className="box" id="box-2-4"></div>
+          <div className="reviewCircles2"><ReviewCircles /></div>
         </div>
         <div className="row" id="row-3">
           <div className="box" id="box-3-1"></div>
           <div className="box" id="box-3-2"></div>
           <div className="box" id="box-3-3"></div>
           <div className="box" id="box-3-4"></div>
+          <div className="reviewCircles3"><ReviewCircles /></div>
         </div>
         <div className="row" id="row-4">
           <div className="box" id="box-4-1"></div>
           <div className="box" id="box-4-2"></div>
           <div className="box" id="box-4-3"></div>
           <div className="box" id="box-4-4"></div>
+          <div className="reviewCircles4"><ReviewCircles /></div>
         </div>
         <div className="row" id="row-5">
           <div className="box" id="box-5-1"></div>
           <div className="box" id="box-5-2"></div>
           <div className="box" id="box-5-3"></div>
           <div className="box" id="box-5-4"></div>
+          <div className="reviewCircles5"><ReviewCircles /></div>
         </div>
         <div className="row" id="row-6">
           <div className="box" id="box-6-1"></div>
           <div className="box" id="box-6-2"></div>
           <div className="box" id="box-6-3"></div>
           <div className="box" id="box-6-4"></div>
+          <div className="reviewCircles6"><ReviewCircles /></div>
         </div>
       </div>
     </div>
---
+++ src/WinnerPopup.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './WinnerPopup.css'
+
+function WinnerPopup({ hideOverlay, message }) {
+
+  const handleShare = async () => {
+    try {
+      if (navigator.share) {
+        await navigator.share({
+          text: message
+        });
+      } else {
+        console.log('Web Share API not supported.');
+      }
+    } catch (error) {
+      console.error('Error sharing:', error);
+    }
+  };
+
+  return(
+    <div id="overlay">
+      <div id="popup">
+        <button type='button' id="closeButton" onClick={() => hideOverlay('#overlay')}>X</button>
+        <h2>Congratulations!</h2>
+        <button type="button" id="shareButton" onClick={handleShare}>Share</button>
+      </div>
+    </div>
+
+  );
+}
+
+export default WinnerPopup;
